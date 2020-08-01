5446b6aeb7859df0abad6951ca08a88344c7e255
faraaz-ahmed <faraazahmed000@gmail.com>
2020-08-02 05:23:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const Picture = require('./picture');
+
+const client = new Discord.Client();
+const pandaBotToken =
+  'NzMxODgxOTAwOTQzMjEyNjY0.XwsgIg.KzxKAwKqRPSWd2d5CZv_BkyAjAA';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+  if (message.content === 'avatar') {
+    // message.reply("nigger here's your avatar");
+    // message.reply(message.author.displayAvatarURL());
+    const picture = new Picture();
+    picture.sendAvatar(message);
+  }
+});
+
+client.login(pandaBotToken);
---
+++ picture.js
@@ -0,0 +1,9 @@
+module.exports = class Picture {
+  constructor() {}
+
+  sendAvatar(message) {
+    // Send the user's avatar URL
+    message.reply("nigger here's your avatar");
+    message.reply(message.author.displayAvatarURL());
+  }
+};
