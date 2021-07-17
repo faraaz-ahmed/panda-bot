168ed0bb7ee2888b3cfc97c7b49ac61321e27896
faraaz-ahmed <faraazahmed000@gmail.com>
2021-07-17 22:07:44 +0530

    feat: implement badword detector

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 const Discord = require('discord.js');
-const Picture = require('./picture');
+const BadWordDetector = require('./bad-words');
 
 const client = new Discord.Client();
 const pandaBotToken =
   'NzMxODgxOTAwOTQzMjEyNjY0.XwsgIg.KzxKAwKqRPSWd2d5CZv_BkyAjAA';
+const testerBotToken =
+  'NzYxNTQ4MDkxNDMwMDEwODkx.X3cM7Q.XgBy5IBrWVnHlavgk2g0AhiCh6E';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (message) => {
-  if (message.content === 'avatar') {
-    // message.reply("nigger here's your avatar");
-    // message.reply(message.author.displayAvatarURL());
-    const picture = new Picture();
-    picture.sendAvatar(message);
+  // if (message.content === 'avatar') {
+  //   // message.reply("nigger here's your avatar");
+  //   // message.reply(message.author.displayAvatarURL());
+  //   const picture = new Picture();
+  //   picture.sendAvatar(message);
+  // }
+  const badWordDetector = new BadWordDetector();
+  if (badWordDetector.detect(message.content)) {
+    message.fetch(message.id).then((msg) => msg.delete());
+    message
+      .reply(
+        'Usage of n word is prohibited, please refrain from using such terms.'
+      )
+      .then((msg) => {
+        msg.delete({ timeout: 6000 /*time unitl delete in milliseconds*/ });
+      })
+      .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+  } else if (!badWordDetector.checkIfAlphanumeric(message.content)) {
+    message.fetch(message.id).then((msg) => msg.delete());
+    message
+      .reply('Please use english alphabets')
+      .then((msg) => {
+        msg.delete({ timeout: 6000 /*time unitl delete in milliseconds*/ });
+      })
+      .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
   }
 });
 
